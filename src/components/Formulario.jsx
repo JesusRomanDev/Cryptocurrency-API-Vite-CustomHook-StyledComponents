7b38867ba7f2e9a9657877609400d7f4d9ddacb9
Jesus Roman <jesus_fime@outlook.es>
2023-09-09 14:24:22 -0600

    Agregando un return en nuestro customHook con un styled component y un parametro

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -22,10 +22,13 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
-    const [ SelectMonedas ] = useSelectMonedas(); //le damos el nombre que queramos a lo que este en el arreglo y luego ponemos nuestro Hook
-    SelectMonedas(); //Lo mandamos llamar y nos imprime el cuerpo de lo que sea la funcion
+    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda'); //le damos el nombre que queramos a lo que este en el arreglo y luego ponemos nuestro Hook, como valor inicial tendra un label/texto y se pasa a la funcion useSelectMonedas que esta en el archivo customHook de useSelectMonedas.jsx
+    //SelectMonedas(); //Lo mandamos llamar y nos imprime el cuerpo de lo que sea la funcion
+    const [ SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda')
   return (
     <form>
+        <SelectMonedas />
+        <SelectCriptomonedas />
 
         <InputSubmit type="submit" value="Cotizar" name="" id="" />
     </form>
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
-const useSelectMonedas = () => { //los custom Hooks no tienen un return
-  const SelectMonedas = () => {
-    console.log('Desde select monedas');
-  }
+const Label = styled.label`
+color: #FFF
+`
+
+const useSelectMonedas = (label) => { //los custom Hooks no tienen un return, los parametros al parecer no tienen las {}
+  const SelectMonedas = () => ( //Reemplazandolo por un return, esto significa que imprimira algo en pantalla
+    <>
+        <Label>{label}</Label>
+        
+    </>
+  )
 
   return [SelectMonedas] //Para que esto sea un Hook que podamos importar en otro lugar, SE NECESITA EL RETURN
 }
