8c68026ecd8245a0fd06847d05794b0d6756b5ab
Jesus Roman <jesus_fime@outlook.es>
2023-09-10 19:09:56 -0600

    Agregando styled components a nuestro componente Resultado, asi como haciendo dinamica la imagen del bitcoin en nuestra pagina

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,17 +1,48 @@
 import styled from '@emotion/styled'
 import React from 'react'
 
+const Contenedor = styled.div`
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+`
+const Imagen = styled.img`
+    display: block;
+    width: 150px;
+`
+
+const Texto = styled.p`
+    font-size: 18px;
+    span{
+        font-weight: 700;
+    }
+`
+
+const Precio = styled.p`
+    font-size: 30px;
+    span{
+        font-weight: 700;
+    }
+`
+
 
 const Resultado = ({resultado}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado;
+
   return (
-    <>
-        <p>El precio es de: <span>{PRICE}</span></p>
-        <p>Precio mas alto del dia: <span>{HIGHDAY}</span></p>
-        <p>Precio mas bajo del dia: <span>{LOWDAY}</span></p>
-        <p>Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-        <p>Ultima actualizacion: <span>{LASTUPDATE}</span></p>
-    </>
+    <Contenedor>
+        {/* El src de la imagen la ponemos con los backticks/template strings para poder buscar la imagen dinamicamente */}
+        <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt='imagen-cripto' />
+        <div>
+            <Precio>El precio es de: <span>{PRICE}</span></Precio>
+            <Texto>Precio mas alto del dia: <span>{HIGHDAY}</span></Texto>
+            <Texto>Precio mas bajo del dia: <span>{LOWDAY}</span></Texto>
+            <Texto>Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+            <Texto>Ultima actualizacion: <span>{LASTUPDATE}</span></Texto>
+        </div>
+    </Contenedor>
   )
 }
 
